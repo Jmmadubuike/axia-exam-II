443a4122733363ac76b0ce2720281a84ac0e2bb9
J Mmadubuike <138705639+Jmmadubuike@users.noreply.github.com>
2024-09-14 09:16:55 +0100

    Update commentRoutes.js

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,13 +2,10 @@ const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
 
-// Create a comment
 router.post("/", commentController.createComment);
 
-// Get all comments
 router.get("/", commentController.getAllComments);
 
-// Get comments for a specific post
 router.get("/post/:postId", commentController.getCommentsByPostId);
 
 module.exports = router;
