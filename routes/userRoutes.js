92fab558bf3cd44256a75e9a846bf5dce3cf07b7
J Mmadubuike <138705639+Jmmadubuike@users.noreply.github.com>
2024-09-27 19:33:14 +0100

    Update userRoutes.js
    I added logout endpoint and delete endpoint.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,4 +8,10 @@ router.post('/register', userController.registerUser);
 // Login user
 router.post('/login', userController.loginUser);
 
+// Logout user
+router.post('/logout', userController.logoutUser);
+
+// Delete user
+router.delete('/delete', userController.deleteUser);
+
 module.exports = router;
